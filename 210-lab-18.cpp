cb40d6dce0deb8d4419eec45aa203d29f2aabc10
7Bssk7 <arhip.fin3@gmail.com>
2025-10-03 14:14:50 -0700

    Update

--- 210-lab-18.cpp
+++ 210-lab-18.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
+void outputMenu();
+
 int main(){
+    int entry;
+    
+    outputMenu();
+    cout << "Choice: ";
+    cin >> entry;
 
     return 0;
+}
+
+void outputMenu(){
+    
 }
