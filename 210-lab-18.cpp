cd0a1b613ca06d8c5d43676d77368d3d1dff5703
7Bssk7 <arhip.fin3@gmail.com>
2025-10-03 14:17:00 -0700

    Update

--- 210-lab-18.cpp
+++ 210-lab-18.cpp
@@ -15,5 +15,7 @@ int main(){
 }
 
 void outputMenu(){
-    
+    cout << "Which linked list method should we use?" << endl;
+    cout << "    [1] New nodes are added at the head of the linked list" << endl;
+    cout << "    [2] New nodes are added at the tail of the linked list" << endl;
 }
